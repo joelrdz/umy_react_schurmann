c9bf1db3a818c816a1173c892f453ba4410aa48c
Joel Rdz <joelrdzh@gmail.com>
2021-10-03 17:01:30 -0500

    Redux set-up

---
+++ 17_redux/src/01-index-intro.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const store = createStore((state = 0, action) => {
+  switch (action.type) {
+    case 'incrementar': {
+      return state + 1
+    }
+    case 'decrementar': {
+      return state - 1
+    }
+    case 'set': {
+      return action.payload
+    }
+    default:
+      return state
+  }
+})
+
+console.log(store.getState())
+store.dispatch({ type: 'whatever' })
+console.log(store.getState())
+store.dispatch({ type: 'incrementar' })
+console.log(store.getState())
+store.dispatch({ type: 'decrementar' })
+console.log(store.getState())
+store.dispatch({ type: 'set', payload: 16 })
+console.log(store.getState())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ 17_redux/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Hola Redux
+    </div>
+  );
+}
+
+export default App;
