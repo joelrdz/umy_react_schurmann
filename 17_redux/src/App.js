e753487d148c8f96b6395393315acfed38eb9003
Joel Rdz <joelrdzh@gmail.com>
2021-10-03 18:12:16 -0500

    react-redux enters

--- 17_redux/src/App.js
+++ 17_redux/src/App.js
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+export const reducer = (state = 0, action) => {
+  console.log({action, state})
+  switch (action.type) {
+    case 'incrementar': {
+      return state + 1
+    }
+    case 'decrementar': {
+      return state - 1
+    }
+    case 'set': {
+      return action.payload
+    }
+    default:
+      return state
+  }
+}
+
 function App() {
+  const [value, setValue] = useState('')
+  const dispatch = useDispatch()
+  const state = useSelector(state => state)
+  const set = () => {
+    dispatch({ type: 'set', payload: value })
+    setValue('')
+  }
   return (
-    <div className="App">
-      Hola Redux
+    <div>
+      <p>Contador: {state}</p>
+      <button onClick={() => dispatch({ type: 'incrementar' })}>Incrementar</button>
+      <button onClick={() => dispatch({ type: 'decrementar' })}>Decrementar</button>
+      <button onClick={set}>Set</button>
+      <input type="number" value={value} onChange={e => setValue(Number(e.target.value))} />
     </div>
   );
 }
